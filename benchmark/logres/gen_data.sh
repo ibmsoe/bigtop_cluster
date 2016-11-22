1bc55a40c0ef5d619dc6e51da2eefa964391ec2b
Matthew Brandyberry <mbrandy@us.ibm.com>
2016-11-22 15:21:44 -0600

    Refactor logres tuning parameters.

--- benchmark/logres/gen_data.sh
+++ benchmark/logres/gen_data.sh
@@ -13,5 +13,19 @@ sudo -u hdfs hdfs dfs -rm -R -skipTrash ${HDFS_DEST}
 sudo -u hdfs hdfs dfs -expunge
 sudo -u hdfs hdfs dfs -mkdir -p ${HDFS_DEST}/LogisticRegression
 
+SPARK_CONFIG_OPTS=(
+    --conf spark.rdd.compress=false
+    --conf spark.network.timeout=600
+    --conf spark.hadoop.dfs.blocksize=512m
+    --conf spark.shuffle.consolidateFiles=true
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer
+    --conf spark.executor.extraJavaOptions=\"-XX:ParallelGCThreads=20 -XX:+AlwaysTenure\"
+    --conf spark.default.parallelism=480
+    --executor-memory 200g
+    --executor-cores 40
+    --total-executor-cores 160
+)
+export SPARK_OPTS="${SPARK_CONFIG_OPTS[@]}"
+
 cd ${BIGTOP_BENCH_DIR}/deps/spark-bench/LogisticRegression/bin
 ./gen_data.sh
--- benchmark/logres/run.sh
+++ benchmark/logres/run.sh
@@ -8,5 +8,19 @@ set -a
 . ${BIGTOP_BENCH_DIR}/bench-env.sh
 set +a
 
+SPARK_CONFIG_OPTS=(
+    --conf spark.rdd.compress=false
+    --conf spark.network.timeout=600
+    --conf spark.hadoop.dfs.blocksize=512m
+    --conf spark.shuffle.consolidateFiles=true
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer
+    --conf spark.executor.extraJavaOptions=\"-XX:ParallelGCThreads=20 -XX:+AlwaysTenure\"
+    --conf spark.default.parallelism=480
+    --executor-memory 200g
+    --executor-cores 40
+    --total-executor-cores 160
+)
+export SPARK_OPTS="${SPARK_CONFIG_OPTS[@]}"
+
 cd ${BIGTOP_BENCH_DIR}/deps/spark-bench/LogisticRegression/bin
 ./run.sh
